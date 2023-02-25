ace3f7ae879f03e382678dbb36d14f3bd1b1610d
Ledio Dylgjeri <lediodylgjeri@gamil.com>
2023-02-25 16:51:02 -0500

    get html elements

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// ------------------- Cached Element References -------------------
+const guessInput = document.querySelector('#guess-input');
+const guessesEl = document.querySelector('#prev-guesses');
+const messageEl = document.querySelector('#message');
+const reseBtn = document.querySelector('#reset-button');
+const prevGuessMsg = document.querySelector('#prev-guesses-msg');
+
+// ------------------- Variables -------------------
+
+let secretNum, guessList, isWinner
+
+// ------------------- Event Listeners -------------------
+
+
+// ------------------- Functions -------------------
